6a4406d546990b1c43c3626fd9923b7a2e9e646c
Vova Korginzkyy <kyper32@ya.ru>
2013-06-03 14:51:21 +0300

    Update Balance.cpp

--- Balance.cpp
+++ Balance.cpp
@@ -10,15 +10,24 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-  setlocale(0, "Russian");
+	setlocale(0, "Russian");
 	Balance myBalance;
-	int t,m,n;
-
+	int t,m,b,v,c;
+	const double n=0.03;
+	
 	cout<<"Введите свой баланс: ";
 	cin>>t;
+	m=t;
+	
+	cout<<endl<<"Введите количество месяцев, через которое вы хотите узнать баланс: ";
+	cin>>v;
 
-	myBalance.setBallance(t);
-	myBalance.displayMessage();
+	myBalance.setBallance(t);/*-------Начальный Баланс---------*/
+	myBalance.setSaver1(m);/*-------Начальный Баланс---------*/
+	myBalance.setProcent(n);/*-------Процент---------*/
+	myBalance.setPrirost(t*n);/*--------Прирост--------*/
+	myBalance.setSaver2(m+(t*n)*(v/12));/*-------Общая сумма за месячный Прирост-------*/
+	myBalance.displayMessage();/*-------Вывод---------*/
 	
 	_getch();
 	return 0;
