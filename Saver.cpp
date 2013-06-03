de02f5298cd0bdab3e6d1ffaf69e841417bd39fd
Vova Korginzkyy <kyper32@ya.ru>
2013-06-03 14:51:40 +0300

    Update Saver.cpp

--- Saver.cpp
+++ Saver.cpp
@@ -1,25 +1,72 @@
-// Balance.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
 #include "join.h"
 #include "Saver.h"
 
-using namespace std;
-
-
-int _tmain(int argc, _TCHAR* argv[])
+Balance::Balance()
 {
-	setlocale(0, "Russian");
-	Balance myBalance;
-	int t,m,n;
-
-	cout<<"Введите свой баланс: ";
-	cin>>t;
-
-	myBalance.setBallance(t);
-	myBalance.displayMessage();
-	
-	_getch();
-	return 0;
+}
+void Balance::displayMessage()
+{
+	cout<<"Текущий баланс: "<<getSaver1()<<endl;
+	cout<<"Общая сумма: "<<getSaver2()<<endl;
+}
+Balance::Balance(int bal, int pri, int vuv)
+{
+	setBallance(bal);
+	setPrirost(pri);
+	setVuvod(vuv);
+}
+void Balance::setBallance(int bal)
+{
+	Ballance=bal;
+}
+int Balance::getBallance()
+{
+	return Ballance;
+}
+void Balance::setVuvod(int vuv)
+{
+	Vuvod=vuv;
+}
+int Balance::getVuvod()
+{
+	return Vuvod;
+}
+void Balance::setPrirost(int pri)
+{
+	Prirost=pri;
+}
+int Balance::getPrirost()
+{
+	return Prirost;
+}
+Balance::Balance(int sav1, int sav2, const double proc)
+{
+	setSaver1(sav1);
+	setSaver2(sav2);
+	setProcent(proc);
+}
+void Balance::setSaver1(int sav1)
+{
+	Saver1=sav1;
+}
+int Balance::getSaver1()
+{
+	return Saver1;
+}
+void Balance::setSaver2(int sav2)
+{
+	Saver2=sav2;
+}
+int Balance::getSaver2()
+{
+	return Saver2;
+}
+void Balance::setProcent(const double proc=0.03)
+{
+	Procent=proc;
+}
+double Balance::getProcent()
+{
+	return Procent;
 }
